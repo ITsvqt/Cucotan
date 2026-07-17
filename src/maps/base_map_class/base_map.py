70f27a29a5dcff17170a47e4867d21b5bce61c31
Tsvetelin Istatkov <c.istatkov@gmail.com>
2026-07-17 17:02:48 +0300

    [Implemented] Map classes. Base map for all maps. Classis Catan map.

---
+++ src/maps/base_map_class/base_map.py
@@ -0,0 +1,153 @@
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from enums import Terrain
+    from enums import PortType
+
+class BaseMap:
+    
+    """
+        Base class for all map layouts.
+        Derrived classes store base class required attributes for concrete map
+        as precalculated constants. Parrent provides validation for the length of the lists.
+        
+        Map generation relies on randomly consuming entries from predefined data lists without repetition.
+    """
+    
+    
+    _INIT_ERROR_KEY = "[On map init]"
+    # BALANCED HEX NUMBER DISTRIBUTION
+    MAX_DOTS_PER_VERTEX = 13
+
+    def __init__(
+        self,
+        
+        # RESOURCE HEXES DATA
+        cnt_land_hex  : int,                            # count of land tiles
+        land_hexes_cordinates: list[ tuple[int, int] ], # coordinates of each land tile
+        terrain_pool  : list[ Terrain ],                # 1 desert, 3 mountains, 3 mines, 4 pastures ...
+        cnt_resource_number    : int,                            # count of numbers for resource tiles
+        numbers_pool   : list[ int ],                    # 1x12, 2x8 ...
+        
+        # SEA HEXES DATA
+        cnt_sea_hex   : int,                            # count of sea tiles
+        sea_hexes_cordinates : list[ tuple[int, int] ], # coordinates of each sea_hex
+        
+        # ADDITIONAL GAME DATA
+        cnt_port      : int,                            # count of ports
+        ports_pool     : list[ PortType ],               # 3x3:1, 1xwheat(2:1)
+        
+        # VALUES FOR VALIDATING DATA VOLUME
+        # TODO: not important right now
+        cnt_hex       : int,                            # for validation of generated data
+        cnt_vertex    : int,                            # for validation of generated data
+        cnt_edge      : int                             # for validation of generated data
+        ):
+        
+        self._ensure_hex_data_lengths_match(
+            cnt_land_hex, land_hexes_cordinates, terrain_pool,
+            cnt_resource_number, numbers_pool,
+            cnt_sea_hex, sea_hexes_cordinates)
+        self._ensure_port_data_length_match(cnt_port, ports_pool)
+        self._ensure_valid_dice_number_pool(numbers_pool)
+        
+        # Land
+        self._cnt_land_hex = cnt_land_hex
+        self._land_hexes_cordinates = land_hexes_cordinates
+        self._terrain_pool = terrain_pool
+        
+        # Sea
+        self._cnt_sea_hex = cnt_sea_hex
+        self._sea_hexes_cordinates = sea_hexes_cordinates
+        
+        # Port
+        self._cnt_port = cnt_port
+        self._port_pool = ports_pool
+        
+        # Expected values for generation
+        self._cnt_hexes = cnt_hex
+        self._cnt_vertices = cnt_vertex
+        self._cnt_edges = cnt_edge
+        
+    
+    
+
+    
+    # INIT DATA VALIDATION METHODS
+    @staticmethod
+    def _ensure_hex_data_lengths_match(
+        cnt_land_hex: int,
+        land_hexes_cordinates: set[tuple[int, int]],
+        hex_terrail_pool: list[Terrain],
+        cnt_number_pool: int,
+        number_pool: list[int],
+        cnt_sea_hex: int,
+        sea_hexes_cordinates: set[tuple[int, int]]
+        ):
+        
+        """ Checks lists of land hexes data for mismatching length. """
+        
+        
+        localization_msg = BaseMap._INIT_ERROR_KEY
+        if cnt_land_hex != len(land_hexes_cordinates):
+            raise ValueError(
+                f"{localization_msg} Illegal land hexes coordinates count!\n"
+                f"Exp : [{cnt_land_hex}]\n"
+                f"Actl: [{len(land_hexes_cordinates)}]"
+                )
+        if cnt_land_hex != len(hex_terrail_pool):
+            raise ValueError(
+                f"{localization_msg} Illegal terrain pool count on map init!\n"
+                f"Exp : [{cnt_land_hex}]\n"
+                f"Actl: [{len(hex_terrail_pool)}]"
+            )
+        if cnt_number_pool != len(number_pool):
+            raise ValueError(
+                f"{localization_msg} Illegal dice numbers for tarrain count!\n"
+                f"Exp : [{cnt_number_pool}]\n"
+                f"Actl: [{len(number_pool)}]"
+            )
+        if cnt_sea_hex != len(sea_hexes_cordinates):
+            raise ValueError(
+                f"{localization_msg} Illegal sea hexes coordinates count!\n"
+                f"Exp : [{cnt_land_hex}]\n"
+                f"Actl: [{len(sea_hexes_cordinates)}]"
+                )
+
+    @staticmethod
+    def _ensure_land_and_sea_hexes_do_not_overlap(land_hexes_cordinate:set, sea_hexes_cordinate:set):
+        """ Checks if land and sea overlap. """
+
+        overlap = land_hexes_cordinate.intersection(sea_hexes_cordinate) 
+        if len(overlap) != 0:
+            overlap_lines = [f"{cordinates}" for cordinates in overlap]
+            raise ValueError(
+                f"{BaseMap._INIT_ERROR_KEY} Land and sea hexes overlap at:\n"
+                f"{'\n'.join(overlap_lines)}"
+                )
+            
+    @staticmethod    
+    def _ensure_port_data_length_match(cnt_port: int, ports_pool: list[PortType]): 
+        """ Checks port_pool for mismatching length. """
+        
+        if cnt_port != len(ports_pool):
+            raise ValueError(
+                f"{BaseMap._INIT_ERROR_KEY} Illegal ports counts\n"
+                f"Exp: {cnt_port}\n"
+                f"Actl: [{len(ports_pool)}]"
+                )
+    
+    @staticmethod
+    def _ensure_valid_dice_number_pool(numbers_pool: list[int]):
+        """ Checks values in the pool of numbers for terrain hexes. """
+        
+        valid_number_values = {2,3,4,5,6,8,9,10,11,12} # possible 2 dice rolls (except 7)
+        
+        for number in numbers_pool:
+            if number not in valid_number_values:
+                raise ValueError(f"{BaseMap._INIT_ERROR_KEY} Illegal number [{number}] in dice numbers pool!\n")
+
+        
+        
+        
---
+++ src/maps/classic_catan_map.py
@@ -0,0 +1,82 @@
+
+from core.maps.base_map_class.base_map import BaseMap
+from enums import Terrain, PortType
+
+class ClassicMap(BaseMap):
+    
+    """ Original base map of Catan. """
+    
+    # CONSTANTS
+      # LAND
+    _CNT_FOREST   = 4  # wood
+    _CNT_PASTURE  = 4  # sheep
+    _CNT_FIELDS   = 4  # wheat
+    _CNT_MOUNTAIN = 3  # ore
+    _CNT_HILLS    = 3  # brick
+    _CNT_DESERT   = 1  # no resource
+    _CNT_LAND_HEX = 19 # expected to match the sum of all different land tiles
+    _LAND_HEXES_CORDINATES = {
+                (-1,-2),( 0,-2),(1,-2),
+            (-2,-1),(-1,-1),(0,-1),(1,-1),
+        (-2, 0),(-1, 0),(0, 0),(1, 0),(2,0),
+            (-2, 1),(-1, 1),(0, 1),(1, 1),
+                (-1, 2),( 0, 2),(1, 2)
+    }
+    _TERRAIN_POOL = (
+        [Terrain.FOREST]     * _CNT_FOREST
+        + [Terrain.PASTURE]  * _CNT_PASTURE
+        + [Terrain.FIELD]    * _CNT_FIELDS
+        + [Terrain.HILL]     * _CNT_HILLS
+        + [Terrain.MOUNTAIN] * _CNT_MOUNTAIN
+        + [Terrain.DESERT]   * _CNT_DESERT
+        )
+    
+      # NUMBERS
+    _CNT_NUMBER = _CNT_LAND_HEX - _CNT_DESERT 
+    _RESOURCE_NUMBERS_POOL = (
+        [3, 4, 5, 6, 8, 9, 10, 11] * 2
+        + [2, 12] * 1
+    )
+    
+      # SEA
+    _CNT_SEA_HEX = 18
+    _SEA_HEXES_CORDINATE = {
+        (0,-3),(1,-3),(2,-3),(3,-3),
+        (3,-2),(3,-1),
+        (2,1),(1,2),(0,3),
+        (-1,3),(-2,3),(-3,3),
+        (-3,2),(-3,1),
+        (-2,-1),(-1,-2),
+        (-1,-3),(2,-2)
+    }
+    
+      # PORTS
+    CNT_GENERIC_PORT = 4
+    CNT_PORT_PER_RESOUCE = 1
+    _CNT_PORT     = 9  # 4 generic + 1 for each resource
+    _PORTS_POOL = (
+        [PortType.GENERIC] * CNT_GENERIC_PORT,
+        [PortType.CLAY] * CNT_PORT_PER_RESOUCE,
+        [PortType.WOOD] * CNT_PORT_PER_RESOUCE,
+        [PortType.SHEEP] * CNT_PORT_PER_RESOUCE,
+        [PortType.GRAIN] * CNT_PORT_PER_RESOUCE,
+        [PortType.ORE] * CNT_PORT_PER_RESOUCE
+    )
+    
+      # GENERATION VALIDATION
+    CNT_HEX = _CNT_LAND_HEX + _CNT_SEA_HEX # 37
+    CNT_VERTEX = 0 # Todo: calculate
+    CNT_EDGE = 0   # Todo: calculate
+    
+    def __init__(self):
+        super().__init__(
+            self._CNT_LAND_HEX,
+            self._LAND_HEXES_CORDINATES,
+            self._TERRAIN_POOL,
+            self._CNT_NUMBER,
+            self._RESOURCE_NUMBERS_POOL,
+            self._CNT_SEA_HEX,
+            self._SEA_HEXES_CORDINATE,
+            self._CNT_PORT,
+            self._PORTS_POOL
+        )
